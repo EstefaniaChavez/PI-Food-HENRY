ae3ecaad1df14f7a71f8af295941be74c71e6697
EstefaniaChavez <estefaniachavez1506@gmail.com>
2022-07-17 19:48:15 -0300

    DB

--- api/src/db.js
+++ api/src/db.js
@@ -30,9 +30,12 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Recipe } = sequelize.models;
+const { Recipe, Diet } = sequelize.models;
 
 // Aca vendrian las relaciones
+Recipe.belongsToMany(Diet, {through: 'diet_recipe'})
+Diet.belongsToMany(Recipe, {through: 'diet_recipe'})
+
 // Product.hasMany(Reviews);
 
 module.exports = {
--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -2,11 +2,49 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('recipe', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+    // defino el modelo
+    sequelize.define('recipe', {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        summary: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        healthScore: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 0,
+                max: 100,
+            }
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        steps: {
+            type: DataTypes.ARRAY(DataTypes.JSON),
+        },
+        score: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            validate: {
+                min: 0,
+                max: 10, 
+            }
+        },
+        createdInDB: {
+            type: DataTypes.BOOLEAN, 
+            allowNull: false,
+            defaultValue: true, 
+        },
+    });
 };
